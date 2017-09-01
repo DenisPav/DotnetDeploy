d36a25e6e1a526c32893ece74f15a17296b124a5
Denis Pavlovic <denis.pav@hotmail.com>
2017-09-01 23:13:45 +0200

    Removed samples as they arent needed

--- pack.ps1
+++ pack.ps1
@@ -1,7 +1,4 @@
 Set-Location $PSScriptRoot
 
-$revNumber = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
-$revNumber = "dev-{0:D4}" -f [convert]::ToInt32($revNumber, 10)
-
-dotnet pack ./src/DotnetDeploy/DotnetDeploy.csproj -o ../../artifacts --version-suffix=$revNumber
+dotnet pack ./src/DotnetDeploy/DotnetDeploy.csproj -o ../../artifacts
 
